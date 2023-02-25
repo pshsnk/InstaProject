321355569104b80d30293882b0b34d0fac29dd31
pshsnk <paska7667@gmail.com>
2023-02-25 21:44:41 +0300

    Add UserService and UserExistExeption

---
+++ inst/src/main/java/com/example/inst/services/UserService.java
@@ -0,0 +1,46 @@
+package com.example.inst.services;
+
+import com.example.inst.entity.User;
+import com.example.inst.entity.enums.ERole;
+import com.example.inst.exeptions.UserExistExeption;
+import com.example.inst.payload.request.SignupRequest;
+import com.example.inst.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    public final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn){
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(ERole.ROLE_USER);
+
+        try{
+            LOGGER.info("Saving User {}", userIn.getEmail());
+            return userRepository.save(user);
+        }catch (Exception e){
+            LOGGER.error("Error: " + e.getMessage());
+            throw new UserExistExeption("The user "+ user.getEmail()+ "is already exist");
+        }
+
+    }
+}
